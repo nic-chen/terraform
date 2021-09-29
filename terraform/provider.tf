bbcbcdba684cbe3751c8b46f0c08179573f1eebc
nic-chen <johz@163.com>
2021-09-30 00:22:52 +0800

    test: remove backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "terraform"
-    storage_account_name = "terraform2"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
